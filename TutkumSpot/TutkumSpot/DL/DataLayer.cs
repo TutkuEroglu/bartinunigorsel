5efd005463b0c3e604bf4a11659a28192532dedf
TutkuEroglu <rrandqua@gmail.com>
2022-06-17 16:25:11 +0300

    my final work

---
+++ TutkumSpot/TutkumSpot/DL/DataLayer.cs
@@ -0,0 +1,492 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace TutkumSpot.DL
+{
+    public static class DataLayer
+    {
+        static MySqlConnection conn = new MySqlConnection(
+            new MySqlConnectionStringBuilder()
+            {
+                Server = "127.0.0.1",
+                Database = "20015222008_db",
+                UserID = "root",
+                Password = "",
+
+            }.ConnectionString
+         );
+        public static int MüşteriEkle(Musteri m)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.MusteriEkle", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@id", m.ID);
+                komut.Parameters.AddWithValue("@ad", m.Ad);
+                komut.Parameters.AddWithValue("@soyad", m.Soyad);
+                komut.Parameters.AddWithValue("@tel", m.Telefon);
+                komut.Parameters.AddWithValue("@mail", m.Mail);
+                komut.Parameters.AddWithValue("@adr", m.Adres);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if(conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+            
+        internal static DataSet MüşteriGetir(string filtre)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut;
+                if (string.IsNullOrEmpty(filtre))
+                {
+                    komut = new MySqlCommand("20015222008_db.MusterilerinHepsi", conn);
+                    komut.CommandType = System.Data.CommandType.StoredProcedure;
+                }
+                else
+                {
+                    komut = new MySqlCommand("20015222008_db.MusterilerBul", conn);
+                    komut.CommandType = System.Data.CommandType.StoredProcedure;
+                    komut.Parameters.AddWithValue("@filtre", filtre);
+                }
+
+                DataSet dataSet = new DataSet();
+                MySqlDataAdapter adp = new MySqlDataAdapter(komut);
+                adp.Fill(dataSet);
+
+                return dataSet;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+
+        internal static int MüşteriGüncelle(Musteri m)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.MusteriGuncelle", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@id", m.ID);
+                komut.Parameters.AddWithValue("@ad", m.Ad);
+                komut.Parameters.AddWithValue("@soy", m.Soyad);
+                komut.Parameters.AddWithValue("@tel", m.Telefon);
+                komut.Parameters.AddWithValue("@mail", m.Mail);
+                komut.Parameters.AddWithValue("@adr", m.Adres);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        internal static int MüşteriSil(string id)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.MusteriSil", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@id", id);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+
+        internal static int UrunEkle(Urun u)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.Urunekle", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@id", u.ID);
+                komut.Parameters.AddWithValue("@ad", u.Ad);
+                komut.Parameters.AddWithValue("@kategori", u.Kategori);
+                komut.Parameters.AddWithValue("@fiyat", u.Fiyat);
+                komut.Parameters.AddWithValue("@stok", u.Stok);
+                komut.Parameters.AddWithValue("@birim", u.Birim);
+                komut.Parameters.AddWithValue("@detay", u.Detay);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        
+        internal static DataSet UrunGetir(string filtre)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut;
+                if (string.IsNullOrEmpty(filtre))
+                {
+                    komut = new MySqlCommand("20015222008_db.UrunlerHepsi", conn);
+                    komut.CommandType = System.Data.CommandType.StoredProcedure;
+                }
+                else
+                {
+                    komut = new MySqlCommand("20015222008_db.UrunBul", conn);
+                    komut.CommandType = System.Data.CommandType.StoredProcedure;
+                    komut.Parameters.AddWithValue("@filtre", filtre);
+                }
+
+                DataSet dataSet = new DataSet();
+                MySqlDataAdapter adp = new MySqlDataAdapter(komut);
+                adp.Fill(dataSet);
+
+                return dataSet;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        internal static int UrunGüncelle(Urun u)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.UrunGuncelle", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@id", u.ID);
+                komut.Parameters.AddWithValue("@ad", u.Ad);
+                komut.Parameters.AddWithValue("@kategori", u.Kategori);
+                komut.Parameters.AddWithValue("@fiyat", u.Fiyat);
+                komut.Parameters.AddWithValue("@stok", u.Stok);
+                komut.Parameters.AddWithValue("@birim", u.Birim);
+                komut.Parameters.AddWithValue("@detay", u.Detay);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        internal static int UrunSil(string id)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_Urunsil", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@id", id);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        internal static int SatisSil(string id)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.SatisSil", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@id", id);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        internal static int SatisEkle(Satis s)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.SatissEkle", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@sid", s.ID);
+                komut.Parameters.AddWithValue("@mid", s.MusteriID);
+                komut.Parameters.AddWithValue("@uid", s.UrunID);
+                komut.Parameters.AddWithValue("@tarih", s.Tarih);
+                komut.Parameters.AddWithValue("@fiyat", s.Fiyat);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+
+        }
+        internal static int OdemeEkle(Odeme o)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.OdemeEkle", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@oid", o.ID);
+                komut.Parameters.AddWithValue("@mid", o.MusteriID);
+                komut.Parameters.AddWithValue("@tarih", o.Tarih);
+                komut.Parameters.AddWithValue("@tutar", o.Tutar);
+                komut.Parameters.AddWithValue("@tur", o.Tur);
+                komut.Parameters.AddWithValue("@aciklama", o.Aciklama);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        internal static int OdemeGüncelle(Odeme o)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.OdemeGuncelle", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@oid", o.ID);
+                komut.Parameters.AddWithValue("@mid", o.MusteriID);
+                komut.Parameters.AddWithValue("@tarih", o.Tarih);
+                komut.Parameters.AddWithValue("@tutar", o.Tutar);
+                komut.Parameters.AddWithValue("@tur", o.Tur);
+                komut.Parameters.AddWithValue("@aciklama", o.Aciklama);
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+        internal static int SatisGüncelle(Satis s)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.Satisguncelle", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@sid", s.ID);
+                komut.Parameters.AddWithValue("@mid", s.MusteriID);
+                komut.Parameters.AddWithValue("@uid", s.UrunID);
+                komut.Parameters.AddWithValue("@tarih", s.Tarih);
+                komut.Parameters.AddWithValue("@fiyat", s.Fiyat);
+
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+
+        internal static DataSet SatisDeyat()
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.SatisDetayi", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+
+
+                DataSet dataSet = new DataSet();
+                MySqlDataAdapter adp = new MySqlDataAdapter(komut);
+                adp.Fill(dataSet);
+
+                return dataSet;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+
+        internal static DataSet OdemeDetay()
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.OdemeDetay", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+
+
+                DataSet dataSet = new DataSet();
+                MySqlDataAdapter adp = new MySqlDataAdapter(komut);
+                adp.Fill(dataSet);
+
+                return dataSet;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+
+        internal static int OdemeSil(string id)
+        {
+            try
+            {
+                if (conn.State != System.Data.ConnectionState.Open)
+                    conn.Open();
+
+                MySqlCommand komut = new MySqlCommand("20015222008_db.Odemesil", conn);
+                komut.CommandType = System.Data.CommandType.StoredProcedure;
+                komut.Parameters.AddWithValue("@oid", id);
+
+
+                int res = komut.ExecuteNonQuery();
+                return res;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                if (conn.State != System.Data.ConnectionState.Closed)
+                    conn.Close();
+            }
+        }
+    }
+}
---
+++ TutkumSpot/TutkumSpot/UI/FrmMusteri.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace TutkumSpot.UI
+{
+    partial class FrmMusteri
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSoy = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAdres = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtTel = new System.Windows.Forms.MaskedTextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(71, 26);
+            this.txtID.Name = "txtID";
+            this.txtID.ReadOnly = true;
+            this.txtID.Size = new System.Drawing.Size(226, 20);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(71, 55);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(226, 20);
+            this.txtAd.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ad";
+            // 
+            // txtSoy
+            // 
+            this.txtSoy.Location = new System.Drawing.Point(71, 78);
+            this.txtSoy.Name = "txtSoy";
+            this.txtSoy.Size = new System.Drawing.Size(226, 20);
+            this.txtSoy.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Soyad";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Telefon";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(71, 130);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(226, 20);
+            this.txtMail.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 133);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(26, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Mail";
+            // 
+            // txtAdres
+            // 
+            this.txtAdres.Location = new System.Drawing.Point(71, 156);
+            this.txtAdres.Multiline = true;
+            this.txtAdres.Name = "txtAdres";
+            this.txtAdres.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtAdres.Size = new System.Drawing.Size(226, 68);
+            this.txtAdres.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Adres";
+            // 
+            // txtTel
+            // 
+            this.txtTel.Location = new System.Drawing.Point(71, 104);
+            this.txtTel.Mask = "\\0(599) 000-0000";
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(226, 20);
+            this.txtTel.TabIndex = 3;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(90, 247);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(75, 23);
+            this.btnOk.TabIndex = 6;
+            this.btnOk.Text = "Tamam";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(195, 247);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "İptal";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // FrmMusteri
+            // 
+            this.AcceptButton = this.btnOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.btnCancel;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtAdres);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSoy);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtAd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmMusteri";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "FrmMusteri";
+            this.Load += new System.EventHandler(this.FrmMusteri_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSoy;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAdres;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.MaskedTextBox txtTel;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
